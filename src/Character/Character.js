27676571399fe7a977b072a40e50abb1f9e84016
FireFly3 <4irelly@gmail.com>
2022-09-15 15:07:41 +0200

    cw

--- src/Character/Character.js
+++ src/Character/Character.js
@@ -1,19 +1,57 @@
-import React, {useState} from 'react';
-import Character from "./Character";
+// import React, {useState} from 'react';
+// import Character from "./Character";
+//
+//
+// const Characters = () => {
+//
+//     let [characters, setCharacters] = useState([]);
+//
+//     fetch('https://rickandmortyapi.com/api/character')
+//         .then(value => value.json())
+//         .then(value => {
+//             setCharacters(value)
+//         });
+//     return (
+//         <div>
+//             {characters.map((character, index)=>(<Character item={character} key={index}/>))}
+//         </div>
+//     );
+// };
 
+export default Characters;
 
-const characters = () => {
+// import React, {useState} from 'react';
+// import User from "../User/User";
+//
+// const Users = () => {
+//
+//     let [users, setUsers] = useState([]);
+//
+//     fetch('https://jsonplaceholder.typicode.com/users')
+//         .then(value => value.json())
+//         .then(value => {
+//             setUsers(value)
+//         });
+//     return (
+//         <div>
+//             {users.map((user, index)=>(<User item={user} key={index}/>))}
+//         </div>
+//     );
+// };
+//
+// export default Users;
 
-    let [characters, setCharacters] = useState([]);
+import React from 'react';
 
-    fetch('https://rickandmortyapi.com/api/character')
-        .then(value => value.json())
-        .then(value => {
-            setCharacter(value)
-        });
+const Character = (props) => {
+    let{character} = props;
     return (
         <div>
-            {characters.map((character, index)=>(<Character item={character} key={index}/>))}
+            <h2>{character.id}-{character.name}</h2>
+            <p>{character.status}</p>
+            <p>{character.species}</p>
+            <p>{character.gender}</p>
+            <img src={character.img} alt=""/>
         </div>
     );
 };
--- src/Characters/Characters.js
+++ src/Characters/Characters.js
@@ -28,3 +28,25 @@ export default Characters;
 // };
 //
 // export default User;
+
+import React, {useState} from 'react';
+import Character from "./Characters";
+
+
+const Characters = () => {
+
+    let [characters, setCharacters] = useState([]);
+
+    fetch('https://rickandmortyapi.com/api/character')
+        .then(value => value.json())
+        .then(value => {
+            setCharacters(value)
+        });
+    return (
+        <div>
+            {characters.map((character, index)=>(<Character item={character} key={index}/>))}
+        </div>
+    );
+};
+
+export default Characters;
