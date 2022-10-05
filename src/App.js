19f0072a8a8d8662b3a4a8b571bf38d6986e179a
FireFly3 <4irelly@gmail.com>
2022-10-05 17:39:20 +0200

    CW 7

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
+import {useDispatch, useSelector} from "react-redux";
+import {useEffect} from "react";
 
 function App() {
-  return (
-    <div className="App">
 
-    </div>
-  );
+    let state = useSelector(state => state.userReducer);
+    console.log(state);
+    let dispatch = useDispatch();
+
+    useEffect(() => {
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(value => value.json())
+            .then(value => {
+                dispatch({type: 'LOAD_USERS', payload: value})
+            });
+    }, [])
+
+    return (
+        <div>
+
+        </div>
+    );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,12 +2,61 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import App from './App';
+import {Provider} from "react-redux";
+import {combineReducers, createStore} from "redux";
 
+
+const userReducer = (state = {users: [], user: null}, action) => {
+    switch (action.type) {
+        case "LOAD_USERS":
+            return {...state, users: action.payload};
+        case "CHOOSE_USER":
+            let id = action.payload;
+            let user = state.users.find(value => value.id === id);
+            return {...state, user: user}
+        default:
+            return state
+    }
+};
+
+const postReducer = (state = {posts: [], post: null}, action) => {
+    switch (action.type) {
+        case "LOAD_POSTS":
+            return {...state, posts: action.payload};
+        case "CHOOSE_POST":
+            let id = action.payload;
+            let post = state.posts.find(value => value.id === id);
+            return {...state, post: post}
+        default:
+            return state
+    }
+};
+
+const commentReducer = (state = {comments: [], comment: null}, action) => {
+    switch (action.type) {
+        case "LOAD_COMMENTS":
+            return {...state, comments: action.payload};
+        case "CHOOSE_COMMENT":
+            let id = action.payload;
+            let comment = state.comments.find(value => value.id === id);
+            return {...state, comment: comment}
+        default:
+            return state
+    }
+};
+
+let reducer = combineReducers({
+    userReducer,
+    postReducer,
+    commentReducer
+})
+
+let store = createStore(reducer);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <Provider store={store}>
+        <App/>
+    </Provider>
 );
 
