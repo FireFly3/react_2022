bc818ce707a33a5a2e0d635817f147e6a90ac942
FireFly3 <4irelly@gmail.com>
2022-10-06 11:29:32 +0200

    HW 7

--- src/components/Users/Users.js
+++ src/components/Users/Users.js
@@ -5,20 +5,20 @@ import {User} from "../User/User";
 import {LOAD_USERS} from "../../reducers/actions";
 
 const Users = () => {
-    let userRed = useReducer()
-    let state = useSelector(state => state.useRed);
-    console.log(state);
+
+    let {users} = useSelector(state => state.useReducer);
+
     let dispatch = useDispatch();
 
     useEffect(() => {
-        userService.getAllUsers().then(value => {
+        userService.getAllUsers().then(({value}) => {
             dispatch({type: LOAD_USERS, payload: value})
         })
-    })
+    },[])
 
     return (
         <div>
-            {state.map(user => <User key={user.id} user={user}/>)}
+            {users.map(user => <User key={user.id} user={user}/>)}
         </div>
     );
 };
